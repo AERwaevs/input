6fefca84ba511042930f63c41f3d9910d7de8479
Hammy Cooper <thehamstarr@gmail.com>
2022-12-08 02:36:28 +0800

    rework Mouse::Code names

--- MouseCodes.h
+++ MouseCodes.h
@@ -7,14 +7,16 @@ namespace AEON
         enum Code : uint8_t
         {
             None        = 0,
-            Left        = BIT( 0 ),
-            Right       = BIT( 1 ),
-            Middle      = BIT( 2 ),
-            Side1       = BIT( 3 ),
-            Side2       = BIT( 4 ),
-            Button5     = BIT( 5 ),
-            Button6     = BIT( 6 ),
-            Button7     = BIT( 7 )
+            Primary     = BIT( 0 ),
+            Secondary   = BIT( 1 ),
+            Tertiary    = BIT( 2 ),
+            Auxilliary1 = BIT( 3 ),
+            Auxilliary2 = BIT( 4 ),
+            Left        = Primary,
+            Right       = Secondary,
+            Middle      = Tertiary,
+            Front       = Auxilliary1,
+            Back        = Auxilliary2
         };
     }
 }

633d5c7989d586b9bc6332ebb0f124dd71ba62b3
Hammy Cooper <thehamstarr@gmail.com>
2023-02-15 22:29:16 +0800

    remove BIND_WITH_ARGS & better_enums include

--- Base.h
+++ Base.h
@@ -19,16 +19,7 @@
 #include "singleton.h"
 #include "Log.h"
 
-//#define BETTER_ENUMS_MACRO_FILE "enum_256.h"
-//#include "../Vendor/better-enums/enum.h"
-
 #define BIT( bit ) 1 << bit
-
-#define BIND_WITH_ARGS( func ) \
-    [this]( auto&&... args ) -> decltype( auto ) \
-    { \
-        return this->func( std::forward< decltype( args ) >( args )... ); \
-    }
 
 #define AEON_TYPE_NAME( T ) \
     template<> constexpr const char*    AEON::GetName( typename T )    noexcept { return #T; } \

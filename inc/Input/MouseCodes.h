084d66fa05d18516a1818f4e8a3801b7c0c99135
Hammy Cooper <thehamstarr@gmail.com>
2023-05-15 00:09:36 +0800

    fix reversion of BIT() ???

--- inc/Input/MouseCodes.h
+++ inc/Input/MouseCodes.h
@@ -2,18 +2,20 @@
 
 namespace AEON::Mouse
 {
-    typedef enum : uint8_t
-    {
-        None        = 0,
-        Primary     = BIT( 0 ),
-        Secondary   = BIT( 1 ),
-        Tertiary    = BIT( 2 ),
-        Auxilliary1 = BIT( 3 ),
-        Auxilliary2 = BIT( 4 ),
-        Left        = Primary,
-        Right       = Secondary,
-        Middle      = Tertiary,
-        Front       = Auxilliary2,
-        Back        = Auxilliary1
-    } Code;
+
+typedef enum : uint8_t
+{
+    None        = 0,
+    Primary     = 1 << 0,
+    Secondary   = 1 << 1,
+    Tertiary    = 1 << 2,
+    Auxilliary1 = 1 << 3,
+    Auxilliary2 = 1 << 4,
+    Left        = Primary,
+    Right       = Secondary,
+    Middle      = Tertiary,
+    Front       = Auxilliary2,
+    Back        = Auxilliary1
+} Code;
+
 }

9cc129f0a866c568cfac0328f73477442bc9baac
AERwaevs <thehamstarr@gmail.com>
2024-11-22 22:47:53 +0800

    binary representation

--- inc/Input/MouseCodes.h
+++ inc/Input/MouseCodes.h
@@ -8,16 +8,16 @@ namespace aer
 enum MouseCode : uint16_t
 {
     MOUSE_None        = 0,
-    MOUSE_Primary     = 1 << 0,
-    MOUSE_Secondary   = 1 << 1,
-    MOUSE_Tertiary    = 1 << 2,
-    MOUSE_Auxilliary1 = 1 << 3,
-    MOUSE_Auxilliary2 = 1 << 4,
+    MOUSE_Primary     = 0b0000100000000,
+    MOUSE_Secondary   = 0b0001000000000,
+    MOUSE_Tertiary    = 0b0010000000000,
+    MOUSE_Auxilliary1 = 0b0100000000000,
+    MOUSE_Auxilliary2 = 0b1000000000000,
     MOUSE_Left        = MOUSE_Primary,
     MOUSE_Right       = MOUSE_Secondary,
     MOUSE_Middle      = MOUSE_Tertiary,
-    MOUSE_Front       = MOUSE_Auxilliary2,
-    MOUSE_Back        = MOUSE_Auxilliary1
+    MOUSE_Forward     = MOUSE_Auxilliary2,
+    MOUSE_Backward    = MOUSE_Auxilliary1
 };
 
 
